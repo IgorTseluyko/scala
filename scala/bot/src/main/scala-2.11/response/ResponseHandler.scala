0359829d79610bdc02d660f5e90ccedad09016c3
igor <tseluyko.igor@gmail.com>
2016-12-06 17:40:56 +0100

    emoji added

--- scala/bot/src/main/scala-2.11/response/ResponseHandler.scala
+++ scala/bot/src/main/scala-2.11/response/ResponseHandler.scala
@@ -1,13 +1,24 @@
 package response
 
-import config.Config.{jasna, undefined}
-import http.HttpManager.sendMessage
+import config.Config._
+import http.HttpManager._
+import model.ReplyMarkupJsonProtocol._
+import model.{Button, ReplyMarkup}
+import spray.json._
+
 
 object ResponseHandler {
 
   def processResponse(chatId: Int, text: String): Unit = {
     text.toLowerCase match {
-      case "snow" | "tatra" | "jasna" => sendMessage(chatId, jasna)
+      case "snow" | "tatra" | "jasna" | "bialka" | "zakopane" | "chopok" =>
+        val markup = ReplyMarkup(Array
+        (Array(
+          Button(s"Jasna $snowflake$snowflake$snowflake", jasna),
+          Button(s"Bialka-Tatrzanska $snowboarder$snowflake$snowflake", bialka)),
+          Array(Button(s"Zakopane $snowflake$snowflake$snowflake", zakopane))
+        )).toJson
+        sendMessage(chatId, "Click on button to see the snow forecast", markup)
       case default => sendMessage(chatId, undefined)
     }
   }
